b14bdfda3146affd7fe803091f19375029612555
NiskashY <roman.pantus@mail.ru>
2022-11-04 10:28:18 +0300

    login page template

---
+++ build-src-Desktop-Debug/CMakeFiles/src.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/src_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/src_autogen.dir/ParseCache.txt"
+  "src_autogen"
+  "CMakeFiles/src.dir/loginwindow.cpp.o"
+  "CMakeFiles/src.dir/loginwindow.cpp.o.d"
+  "CMakeFiles/src.dir/main.cpp.o"
+  "CMakeFiles/src.dir/main.cpp.o.d"
+  "CMakeFiles/src.dir/src_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/src.dir/src_autogen/mocs_compilation.cpp.o.d"
+  "src"
+  "src.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/src.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
